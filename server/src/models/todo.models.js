885efaf0b942dcecc6035252a6f9189ccca1d598
Muhammad Hussnain Shaker <mhussnainshaker251@gmail.com>
2024-07-07 23:08:31 +0500

    Added model schema for todo, added prettier and nodemon

---
+++ server/src/models/todo.models.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose'
+
+const todoSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String
+    },
+    status: {
+        type: String,
+        enum: ['pending', 'completed'],
+        default: 'pending'
+    }
+}, {timestamps: true})
+
+export const Todo = mongoose.model("Todo", todoSchema)
